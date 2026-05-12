c56d2aef577fbbe9c61aa8dd67f86034b6de3a77
Vinay Ankola <vinay.ankola@gmail.com>
2026-05-11 22:34:46 -0400

    Emotion Detection function added

---
+++ emotion_detection.py
@@ -0,0 +1,14 @@
+import requests
+
+def emotion_detector(text_to_analyze):
+
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/" + \
+         "NlpService/EmotionPredict"
+    header = {"grpc-metadata-mm-model-id":
+              "emotion_aggregated-workflow_lang_en_stock"}
+    inputjson = { "raw_document": { "text": text_to_analyze } }    
+
+    response = requests.post(url, json=inputjson, headers=header)
+    return response.text
+
+
