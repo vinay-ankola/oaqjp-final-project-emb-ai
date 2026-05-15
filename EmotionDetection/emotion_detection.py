9f77e5f0c42b12c74de898f24fdc84588f226322
Vinay Ankola <vinay.ankola@gmail.com>
2026-05-15 00:02:29 -0400

    Modified for error handling again

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -4,19 +4,28 @@ def emotion_detector(text_to_analyze):
 
     url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/" + \
          "NlpService/EmotionPredict"
+
     header = {"grpc-metadata-mm-model-id":
               "emotion_aggregated-workflow_lang_en_stock"}
+
     inputjson = { "raw_document": { "text": text_to_analyze } }    
 
     response = requests.post(url, json=inputjson, headers=header)
+
+    if response.status_code == 400:
+        emotions = {
+            'anger': None, 
+            'disgust': None, 
+            'fear': None, 
+            'joy': None, 
+            'sadness': None, 
+            'dominant_emotion': None
+        }
+        return emotions
 
     if response.status_code == 200:
         formatted_response = json.loads(response.text)
         emotions = formatted_response['emotionPredictions'][0]['emotion']
         dominant_emotion = max(emotions, key=emotions.get)
         emotions['dominant_emotion'] = dominant_emotion
-    elif response.status_code == 400:
-        emotions = {'anger': None, 'disgust': None, 'fear': None, 
-                    'joy': None, 'sadness': None, 'dominant_emotion': None}
-
-    return emotions
+        return emotions
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+""" Module server.py """
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,11 +6,12 @@ app = Flask("Emotion Detector")
 
 @app.route('/emotionDetector')
 def emot_detector():
+    """ Function emot_detector """
     text_to_analyze = request.args.get("textToAnalyze")
     response = emotion_detector(text_to_analyze)
 
-    if response['dominant_emotion'] == None:
-        return "Invalid text! Please try again!"
+    if response['dominant_emotion'] is None:
+        return "Invalid input! Try again."
 
     dominant_emotion = response['dominant_emotion']
     response.popitem()
@@ -18,11 +20,13 @@ def emot_detector():
     last_item = {key: value}
     formatted_response = str(response_subset).strip("{}")
     formatted_last_item = str(last_item).strip("{}")
-        
-    return f"For the given statement, the system response is {formatted_response} and {formatted_last_item}. The dominant emotion is {dominant_emotion}"
+
+    return f"For the given statement, the system response is {formatted_response} and" \
+           f" {formatted_last_item}. The dominant emotion is {dominant_emotion}"
 
 @app.route('/')
 def render_index_page():
+    """ Function render_index_page """
     return render_template("index.html")
 
 if __name__ == "__main__":
